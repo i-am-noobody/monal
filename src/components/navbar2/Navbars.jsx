7a06f937223fe0391872f1c581a9093e248d47fd
i-am-noobody <sandeepadh421@gmail.com>
2024-07-12 09:18:07 +0545

    Latest commit

--- src/components/navbar2/Navbars.jsx
+++ src/components/navbar2/Navbars.jsx
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
-import { Link, NavLink, useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { FaBars } from "react-icons/fa";
 import { ImCross } from "react-icons/im";
 import "./navbar.css";
 
 function Navbars() {
-  const navigate = useNavigate();
   const [mobile, setMobile] = useState(false);
   const [active, setActive] = useState(1);
 
@@ -16,12 +15,7 @@ function Navbars() {
   return (
     <nav className="nav main-nav-container">
       <div className="logoDiv">
-        <img
-          src="Final Logo-01-01.png"
-          alt="logo"
-          className="img"
-          onClick={() => navigate("/")}
-        />
+        <img src="Final Logo-01-01.png" alt="logo" className="img" />
       </div>
       <ul
         className={mobile ? "nav-links-mobile" : "nav-links"}
--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
-
-import SecondPage from "./pages/SecondPage.jsx";
-import FifthPage from "./pages/FifthPage.jsx";
-import SixthPage from "./pages/SixthPage.jsx";
-import ThirdPge from "./pages/ThirdPge.jsx";
-import Layout from "./components/Layout.jsx";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/menu" element={<SecondPage />} />
-        <Route path="/gallery" element={<ThirdPge />} />
-        <Route path="/about" element={<FifthPage />} />
-        <Route path="/contact" element={<SixthPage />} />
-        <Route path="/layout" element={<Layout />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
